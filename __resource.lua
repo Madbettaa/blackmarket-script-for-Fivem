c3a4f1b18acdec6093e5c0ee9582042850d63a7c
Madbettaa <121950191+Madbettaa@users.noreply.github.com>
2024-04-09 01:45:57 +0000

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -3,13 +3,12 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 ui_page 'html/laptop.html'
 
 client_scripts {
-    '@np-lib/client/cl_main.lua',
     'client/main.lua',
+    'client/util.lua',
 }
 
 server_scripts {
     '@np-lib/server/sv_main.lua',
-    'server/main.lua',
 }
 
 files {
